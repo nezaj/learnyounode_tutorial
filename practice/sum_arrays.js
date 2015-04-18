b10ea455babc9393b25f8293aa2a165b307f4658
Joe Averbukh <joeaverbukh@gmail.com>
2015-04-19 00:03:55 +0700

    Minor tweaks to sum_arrays

--- practice/sum_arrays.js
+++ practice/sum_arrays.js
@@ -18,7 +18,7 @@ function sumArrays(arr) {
         // sum the elements in the array
         for (var x = 0; x < arr[i].length; x++) {
             sum += arr[i][x];
-            }
+        }
 
         // add this sum to the total
         total += sum;
@@ -29,9 +29,6 @@ function sumArrays(arr) {
 }
 
 function sumArraysRecursive(arr) {
-
-    console.log("Passed array is: " + arr);
-
     // Base case: empty array returns 0
     if (arr.length == 0) {
           return 0;
@@ -44,26 +41,25 @@ function sumArraysRecursive(arr) {
         sum += first_arr[i]
     }
 
-    helpful_message = "Current sum is " + sum + " Array now is: " + arr;
-    console.log(helpful_message);
-
     return sum += sumArraysRecursive(arr)
 }
 
 var error_msg = "Error, expected " + expected + " but got " + actual + "\n\n"
 
-// var arr = [[1,2,3], [4, 5, 6], [7, 8, 9]]
-// var expected = 45
-// var actual = sumArrays(arr)
-// var error_msg = "Error, expected " + expected + " but got " + actual + "\n\n"
-// assert(actual == expected, error_msg)
-// console.log(actual);
-//
-// var arr = []
-// var actual = sumArrays(arr)
-// var expected = 0
-// assert(actual == expected, error_msg)
-// console.log(actual);
+// sumArrays returns 0 for the empty array
+var arr = []
+var actual = sumArrays(arr)
+var expected = 0
+assert(actual == expected, error_msg)
+console.log(actual);
+
+// sumArrays works for multidimensional arrays
+var arr = [[1,2,3], [4, 5, 6], [7, 8, 9]]
+var expected = 45
+var actual = sumArrays(arr)
+var error_msg = "Error, expected " + expected + " but got " + actual + "\n\n"
+assert(actual == expected, error_msg)
+console.log(actual);
 
 // sumArraysRecursive returns 0 for the empty array
 var arr = [];
@@ -72,6 +68,7 @@ var expected = 0
 assert(actual === expected, error_msg)
 console.log(actual);
 
+// sumArraysRecursive works for multidimensional arrays
 var arr = [[1,2,3], [4, 5, 6], [7, 8, 9]]
 var expected = 45
 var actual = sumArraysRecursive(arr)
